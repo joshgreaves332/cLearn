a481bd66c83c27cd76f81482c9dd106772dff01d
joshgreaves332 <joshgreaves332@Gmail.com>
2019-07-08 18:41:33 +0200

    Commiting simple Function code

---
+++ simFunc.cpp
@@ -0,0 +1,45 @@
+//Simple Function
+
+#include <iostream>
+#include <string>
+#include <sstream>
+
+
+using namespace std; //this contains the cout cin variables
+
+int getSum(int a, int b){
+		int result;
+		result = a+b; // + '\n';
+		return result;
+	}
+
+
+int main() {
+	
+	stringstream strOutput;
+	stringstream strSize;
+	int input1;
+	int input2;
+	int intOutput;
+	int intSizeOutput;
+	
+	printf("Please input starting number: \n");
+	cin>>input1;
+	printf("Please input number to add to it: \n");
+	cin>>input2;
+	
+	intOutput = getSum(input1, input2);
+	intSizeOutput = sizeof(short int);
+	
+	strOutput << intOutput;
+	strSize << intSizeOutput;  //It seems int is equiv to long int
+	
+	cout << "Total is: " << strOutput.str() << "\n";
+	cout <<  "Size of Datatype is: " << strSize.str() << "\n";
+
+	
+	return 0;
+	
+}
+
+
